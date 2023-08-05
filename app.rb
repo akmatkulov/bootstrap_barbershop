1b59cb8204c3ac19b667e694c24f8993b8242f90
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2023-08-05 17:12:37 +0600

    Complete 21

--- app.rb
+++ app.rb
@@ -2,7 +2,49 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "BarnerShosssp"			
+end
+get '/admin' do
+  erb :admin
+end
+
+post '/admin' do
+   @login = params['login']
+  @password = params['pass']
+
+  if @login == 'admin' && @password == 'secret'
+    @message = File.read('public/users.txt')
+    @title = 'Textfile'
+    erb :message
+  else
+    @title = "Access denied"
+    @message = "Try at later"
+    erb :message
+  end
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/visit' do
+  erb :visit
+end
+#Отправить данные
+post '/visit' do
+  @name = params['name']
+  @phone_number = params['phone_number']
+  @date_time = params['date_time']
+  @barber = params['barber']
+
+  f = File.open 'public/users.txt', 'a'
+  f.write "User: #{@name}, Barber: #{@barber}, Phone: #{@phone_number}, Date and Time: #{@date_time}\n"
+  f.close
+
+  erb :visit
+end
+
+get '/contacts' do
+  erb :contacts
 end
